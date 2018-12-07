e3d1e5b2e8fe1915c966d82e941cccc44c82f15d
Michal Jez <Michal.G.Jez@gmail.com>
2018-12-07 14:31:44 -0500

    Fixed build script

---
+++ scripts/clear-lib.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const directory = 'lib';
+
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), err => {
+      if (err) throw err;
+    });
+  }
+});
