d5e617281c248f2627366656373af893540ad037
Michal Jez <Michal.G.Jez@gmail.com>
2018-12-15 02:15:45 -0500

    Updated while test

--- test/while.spec.jsx
+++ test/while.spec.jsx
@@ -1,10 +1,11 @@
+// @ts-check
 import * as React from "react";
 import { expect } from "chai";
 import { shallow, configure, render, mount } from "enzyme";
 import { ForIn, While } from "../lib";
 
-describe('<While />', () => {
-  it('Terminates immediately on false', () => {
+describe("<While />", () => {
+  it("Terminates immediately on false", () => {
     const component = (
       <While test={() => false}>{
         () => <h1>Never rendered</h1>
@@ -15,5 +16,20 @@ describe('<While />', () => {
 
     const s = shallow(component);
     expect(s.html()).to.equal("");
+  });
+  it("Loops until false", () => {
+    let i;
+    const component = (
+      <While test={() => i > 0}>{
+        () => <h1>{i--}</h1>
+      }</While>
+    );
+    i = 3;
+    const r = render(component);
+    expect(r).to.have.lengthOf(3);
+
+    i = 3;
+    const s = shallow(component);
+    expect(s.html()).to.equal("<h1>3</h1><h1>2</h1><h1>1</h1>");
   })
 })
