6643b7437aa304132fedcee9931ec1b5ddc696a0
Michal Jez <Michal.G.Jez@gmail.com>
2018-12-14 21:13:45 -0500

    Added While test

--- test/while.spec.jsx
+++ test/while.spec.jsx
@@ -1,2 +1,19 @@
 import * as React from "react";
+import { expect } from "chai";
+import { shallow, configure, render, mount } from "enzyme";
 import { ForIn, While } from "../lib";
+
+describe('<While />', () => {
+  it('Terminates immediately on false', () => {
+    const component = (
+      <While test={() => false}>{
+        () => <h1>Never rendered</h1>
+      }</While>
+    );
+    const r = render(component);
+    expect(r).to.have.lengthOf(0);
+
+    const s = shallow(component);
+    expect(s.html()).to.equal("");
+  })
+})
